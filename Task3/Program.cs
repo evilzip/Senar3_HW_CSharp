94327134f5364d43540ac5ca4813202f9f98c9c0
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-20 17:24:19 +0300

    Task3: solved

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вывести третью степень всех чисел в интервале от 1 до вводимого N
+
+int InputIn(string message)
+{
+    System.Console.Write(($"{message}=>"));
+    int value;
+    bool isCorrect = int.TryParse(Console.ReadLine(), out value);
+    if (isCorrect)
+    {
+        if (value>0)
+         {
+            return value;
+         }
+         System.Console.WriteLine("It is not a positive number");
+        Environment.Exit(-2);
+    }
+    System.Console.WriteLine("It is not a number");
+    Environment.Exit(-1);   
+    return 0;
+}
+
+int userInput = InputIn("Enter your positive integer value");
+
+string resultString = String.Empty;
+
+for (int i = 1; i <= userInput; i++)
+{    
+    resultString = resultString + ($"{Math.Pow(i, 3)},");    
+}
+
+resultString = resultString.TrimEnd(','); // удаление последней запятой в финальном выводt
+System.Console.WriteLine(resultString);
